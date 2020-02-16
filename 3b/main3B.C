eb2bc884fd8b8056f16e534f0797d0694f3e3434
Mamtaj Akter <mamtajakter4@gmail.com>
2020-02-16 02:21:48 -0800

    final 3b project submitted

--- 3b/main3B.C
+++ 3b/main3B.C
@@ -3,15 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-    Image img, img2, img3, img4, img5, img6, img7, img8;
+  Image img, img2, img3, img4, img5, img6, img7, img8;
 
-    ReadImage(argv[1], img);
-    // HalfSize(img, img2);
-    // LeftRightCombine(img2, img2, img3);
-    // TopBottomCombine(img3, img3, img4);
-    // HalfSize(img4, img5);
-    // LeftRightCombine(img5, img2, img6);
-    // TopBottomCombine(img6, img3, img7);
-    // Blend(img7, img, 0.795, img8);
-    WriteImage(argv[2], img);
+  ReadImage(argv[1], img);
+  HalfSize(img, img2);
+  LeftRightCombine(img2, img2, img3);
+  TopBottomCombine(img3, img3, img4);
+  HalfSize(img4, img5);
+  LeftRightCombine(img5, img2, img6);
+  TopBottomCombine(img6, img3, img7);
+  Blend(img7, img, 0.8, img8);
+  WriteImage(argv[2], img8);
 }
